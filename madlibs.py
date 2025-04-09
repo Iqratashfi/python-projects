7a3e2e1b66e8ff745aa9470e076622923f67dda5
Iqra Creates <150584027+Iqratashfi@users.noreply.github.com>
2025-04-10 00:49:02 +0530

    Add Mad Libs program using user input

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+adjective1 = input("Enter adjective to describe your friend: ")
+adjective2 = input("Enter another adjective to describe the animal: ")
+animal = input("Enter an animal: ")
+random_noise = input("Enter a random noise: ")
+
+
+print("Today, my " + adjective1 + " friend and I went to the zoo.")
+print("We saw a " + adjective2 + " " + animal + " jumping up and down in its tree yelling ")
+print(random_noise + "!!! at the top of its lungs.")
+
